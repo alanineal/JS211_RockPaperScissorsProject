5428f78085d6ccbc98808e780351017fca6c5684
alanineal <imthealani@gmail.com>
2022-07-13 10:12:31 -0500

    updating code

--- main.js
+++ main.js
@@ -14,12 +14,118 @@ const rl = readline.createInterface({
 // the function that will be called by the unit test below
 const rockPaperScissors = (hand1, hand2) => {
 
-  // Write code here
-  // Use the unit test to see what is expected
+  // should scrub input to ensure lowercase with "trim'ed whitespace
+  hand1 = hand1.toLowerCase()
+  hand2 = hand2.toLowerCase()
+
+  hand1 = hand1.trim()
+  hand2 = hand2.trim()
+
+
+//Compare User1 input to User2 input.
+
+ if ( (hand1 == 'rock' && hand2== 'rock') || (hand1 == 'paper' && hand2== 'paper') || (hand1 == 'scissors' && hand2== 'scissors')) {
+
+    // console.log("It's a tie!")
+
+    return "It's a tie!"
+ 
+  }else if ( (hand1 == 'rock' && hand2== 'scissors') || (hand1 == 'paper' && hand2== 'rock') || (hand1 == 'scissors' && hand2== 'paper'))
+ // console.log("Hand one wins!")
+
+
+ 
+    return "Hand one wins!"
+  } else if ( (hand1 == 'paper' && hand2== 'scissors') || (hand1 == 'rock' && hand2== 'paper') || (hand1 == 'scissors' && hand2== 'rock'))
+  
+
+
+if (hand1 === hand2) {
+ 
+  return "It's a tie!"
+  } else if (hand1 === "rock"){
+    if (hand2 === "paper") {
+
+      return "Hand two wins!"
+    } else {
+      
+      return "Hand one wins!"
+    }
+  } else if (hand1 === "paper") {
+     if (hand2 === "rock"){
+      return "Hand two wins!"
+     }
+  }
+    }
+
+}
+}
+
+
+
 
 }
 
-// the first function called in the program to get an input from the user
+// If User1 input is 'paper' and User2 input is 'rock', User1 wins.
+// If User1 input is 'paper' and User2 input is 'scissors', User2 wins.
+
+//If User1 input is 'scissors' and User2 input is 'paper', User1 wins.
+//If User1 input is 'scissors' and User2 input is 'rock', User2 wins. 
+
+
+
+
+
+ 
+
+  console.log("It's a tie!")
+
+
+  // it('it should detect which hand won' hand1 wins
+    //If User1 input is 'rock' and User2 input is 'scissors', User1 wins.
+    //If User1 input is 'paper' and User2 input is 'rock', User1 wins. 
+   //If User1 input is 'scissors' and User2 input is 'paper', User1 wins.
+
+  console.log ("Hand one wins!")
+
+  return "Hand one wins!"
+
+
+
+
+
+
+
+
+
+
+
+
+// it('it should detect which hand won' hand2 wins
+  //If User1 input is 'rock' and User2 input is 'paper', User2 wins.
+  //If User1 input is 'paper' and User2 input is 'scissors', User2 wins. 
+  //If User1 input is 'scissors' and User2 input is 'rock', User2 wins.
+
+
+
+  console.log("Hand twon wins!")
+
+
+    return "Hand two wins!"
+
+
+
+} else {
+    getPrompt()
+
+
+}
+}
+
+
+
+
+ // the first function called in the program to get an input from the user
 // to run the function use the command: node main.js
 // to close it ctrl + C
 function getPrompt() {
@@ -34,7 +140,7 @@ function getPrompt() {
 // Unit Tests
 // to use them run the command: npm test main.js
 // to close them ctrl + C
-if (typeof describe === 'function') {
+
 
   // most are notes for human eyes to read, but essentially passes in inputs then compares if the function you built returns the expected output.
   describe('#rockPaperScissors()', () => {
